d86980cfa81fba29732d78111eec0338bef19ad2
Jason Wang <jason.wang1700@gmail.com>
2025-12-12 15:40:35 -0500

    incorporated eel into frontend and backend, can start app through main.py now!

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import eel
 from backend.initialization import initialization as start
 
-start.initialization()
+eel.init('frontend')
+
+if __name__ == "__main__":
+    eel.start('index.html', size=(1000, 800))
+    start.initialization()
