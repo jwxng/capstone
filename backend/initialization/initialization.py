846b85edcdda8b24c9d9235f37a405c27a20e359
Jason Wang <jason.wang1700@gmail.com>
2026-01-08 17:05:19 -0500

    simplifying settings

--- backend/initialization/initialization.py
+++ backend/initialization/initialization.py
@@ -2,18 +2,11 @@ from backend.settings import settings
 
 def initialization():
     user_settings = settings.settings
-    if not user_settings.data["app_initialized"]:
+    if not user_settings.data["terms_agreed"]:
         # INCORPORATE FRONTEND IN THIS BLOCK
-        print("Await user input on \"Get Started\" screen.")
-
         print("Await user agreeing to terms.")
         user_settings.data["terms_agreed"] = True # must be True to proceed
-
-        print("Await webcam calibration")
-        user_settings.data["webcam_calibrated"] = True # must be True to proceed
-
-        print("Await user responses to notification settings.")
-
         user_settings.save_data()
-        print("Checking if app is fully set up...")
-        user_settings.check_setup_complete()
+
+    print("Checking if app is fully set up...")
+    user_settings.check_setup_complete()
--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -5,43 +5,38 @@ CONFIG_FILE = 'settings.json'
 
 class Settings:
     def __init__(self):
-        print("Initializing config settings, loading default values.")
+        print("Initializing settings file.")
         self.data = {
-            "app_initialized": False,
             "terms_agreed": False,
-            "webcam_calibrated": False,
-            "sound_enabled": False,
-            "alerts_enabled": False,
-            "notifs_enabled": False,
         }
         self.load_data()
 
     def load_data(self):
-        print("Checking for existing config settings...")
+        print("Checking for existing settings...")
         if os.path.exists(CONFIG_FILE):
             try:
                 with open(CONFIG_FILE, 'r') as f:
                     existing_data = json.load(f)
                     self.data.update(existing_data)
-                    print("Existing config settings loaded.")
+                    print("Existing settings loaded.")
             except Exception as e:
-                print(f"Error loading config settings: {e}")
+                print(f"Error loading settings: {e}")
         else:
-            print("No config settings file found.")
+            print("No settings file found.")
 
     def save_data(self):
-        print("Saving settings config...")
+        print("Saving settings...")
         try:
             with open(CONFIG_FILE, 'w') as f:
                 json.dump(self.data, f, indent=4)
-                print("Configuration saved.")
+                print("Settings saved.")
         except Exception as e:
-            print(f"Error saving config: {e}")
+            print(f"Error saving settings: {e}")
 
+    # remove?
     def check_setup_complete(self):
-        if self.data["terms_agreed"] and self.data["webcam_calibrated"]:
-            print("App is fully set up.")
-            self.data["app_initialized"] = True
+        if self.data["terms_agreed"]:
+            print("Terms agreed.")
             self.save_data()
 
 settings = Settings()
